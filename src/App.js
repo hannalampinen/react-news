81bf945b1bfe9022a93a345ba6a353710b71d846
hannalampinen <n0laha00@students.oamk.fi>
2021-09-30 15:40:14 +0300

    news tehtävä valmis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import Details from './Details.js';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const URL = 'https://newsapi.org/v2'
+  const APIKEY = '43b63cdb1def4c3685d9107731a2fbf5';
+
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null); // valittu uutinen
+
+  useEffect(() => {
+    const criteria = 'top-headlines?country=us&catecory=entertainment';
+    const address = URL + '/' + criteria + '&apikey=' + APIKEY;
+
+    fetch(address)
+    .then (res => res.json())
+    .then (
+      (result) => {
+        setError(null);
+        setIsLoaded(true);
+        if (typeof result.articles === 'undefined') {
+          setError({"message" : "Error retrieving news"});
+        } else {
+          setItems(result.articles);
+        }
+      }, (error) => {
+        setError(error);
+        setIsLoaded(true);
+        setItems([]);
+      }
+    )
+  }, [])
+
+  function close() {
+    setSelectedItem(null);
+  }
+
+  if (selectedItem != null) {
+    return <Details 
+      title={selectedItem.title}
+      image={selectedItem.urlToImage}
+      description={selectedItem.description}
+      close={close}
+    ></Details>;
+  } else if (error) {
+    return <p>{error.message}</p>
+  } else if (!isLoaded) {
+    return <p>Loading...</p>
+  } else {
+    return (
+    <div>
+      {items.map(item => (
+        <div key={item.title} onClick={e => setSelectedItem(item)}>
+          <h3>{item.title}</h3>
+          <img src={item.urlToImage}></img>
+          <p>{item.description}</p>
+        </div>
+    ))}
     </div>
-  );
+    )
+  }
 }
 
 export default App;
---
+++ src/Details.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Details(props) {
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <img src={props.image}></img>
+            <p>{props.description}</p>
+            <a href='#' onClick={props.close}>Back to news</a>
+        </div>
+    )
+}
+
